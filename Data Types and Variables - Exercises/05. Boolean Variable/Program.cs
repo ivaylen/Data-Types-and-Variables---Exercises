a3540f65e9c990112160307a0fab9d048838a6b7
ivaylen <ivaylen@abv.bg>
2017-10-24 17:15:28 +0300

    DataTypesAndVariables

---
+++ Data Types and Variables - Exercises/05. Boolean Variable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Boolean_Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            bool b = Convert.ToBoolean(a);
+            if (b)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
---
+++ Data Types and Variables - Exercises/10. Centuries to Nanoseconds/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Centuries_to_Nanoseconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            int year = centuries * 100;
+            int days = (int)(year * 365.2422);
+            long hours = days * 24;
+            long minutes = hours * 60;
+            long seconds = minutes * 60;
+            long milliseconds = seconds * 1000;
+            long microseconds = milliseconds * 1000;
+            decimal nanoseconds = microseconds * 1000m;
+
+            Console.WriteLine($"{centuries} centuries = {year} years = {days} days = {hours} hours = {minutes} minutes = {seconds} seconds = {milliseconds} milliseconds = {microseconds} microseconds = {nanoseconds} nanoseconds");
+        }
+    }
+}
